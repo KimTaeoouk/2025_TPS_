d160d5705550c1f9e34a98374c4dec8e988aad59
KimTaeoouk <ukkim088@gmail.com>
2025-06-19 21:48:04 +0900

    2222

--- Assets/Scripts/WeaponController.cs
+++ Assets/Scripts/WeaponController.cs
@@ -4,14 +4,20 @@ public class WeaponController : MonoBehaviour
 {
     public WeaponBase currentWeapon;
     public PlayerInputReader input;
+    public ParticleSystem particle;
 
+    private void Start()
+    {
+        particle.Stop();
+    }
     void Update()
     {
         if (input.IsFiring && currentWeapon != null && currentWeapon.CanFire() && input.IsAiming)
         {
             Debug.Log("무기 발사 시도");
             currentWeapon.Fire();
+            particle.Play();
+
         }
-        
     }
 }
